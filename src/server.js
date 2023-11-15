12260e41f2dc0e6063e7fbe3375a6194dad78802
Thiago Koch <tkoch.dev@gmail.com>
2023-11-15 16:37:19 -0300

    feat: install cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 require("dotenv/config");
 require("express-async-errors");
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const AppError = require('./utils/AppError');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
